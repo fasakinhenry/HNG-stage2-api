affa51534f83867defaf50142159064805c54df2
Fasakin Henry <fasakinhenry@gmail.com>
2026-04-24 01:24:30 +0100

    feat: handle environment safeguards

--- src/config/env.js
+++ src/config/env.js
@@ -3,12 +3,14 @@ import { envFile } from './paths.js'
 
 dotenv.config({ path: envFile })
 
+const normalizeOrigin = (origin) => origin.replace(/\/+$/, '')
+
 export const env = {
   nodeEnv: process.env.NODE_ENV ?? 'development',
   port: Number(process.env.PORT ?? 5000),
   mongoUri: process.env.MONGODB_URI ?? '',
   jwtSecret: process.env.JWT_SECRET ?? '',
-  clientOrigin: process.env.CLIENT_ORIGIN ?? 'http://localhost:5173',
+  clientOrigin: normalizeOrigin(process.env.CLIENT_ORIGIN ?? 'http://localhost:5173'),
   cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME ?? '',
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY ?? '',
   cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET ?? '',
